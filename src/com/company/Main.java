b299993e50983fca581fdc10b304efca4ac2191d
bladekeys <vlad.los700@gmail.com>
2021-09-07 10:39:03 +0300

    prac 1.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello world 11");
+        int[] a = {1, 4 , 6, 8, 40, 123 ,46, 6787, 586868};
+        int b=0;
+        for(int i=0; i<a.length;i++) {
+            b+=a[i];
+        }
+        System.out.println(b);
+
+        b=0;
+        int i=0;
+        while (i<a.length){
+            b+=a[i];
+            i++;
+        }
+        System.out.println(b);
+
+        b=0;
+        i=0;
+        do{
+            b+=a[i];
+            i++;
+        } while(i<a.length);
+        System.out.println(b);
     }
 }
